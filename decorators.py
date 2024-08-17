9848a6b607a7ad75ab8b60918249a8f69eeb4c52
Jalal Uddin <jalaluddin4@outlook.com>
2024-08-17 10:03:23 +0600

    Decorators

---
+++ decorators.py
@@ -0,0 +1,128 @@
+#Nested Function
+def outer(x):
+    def inner(y):
+        return x + y
+    return inner
+
+add_five = outer(5)
+result = add_five(6)
+print(result)  # prints 11
+
+# Output: 11
+
+#Pass Function as Argument
+def add(x, y):
+    return x + y
+
+def calculate(func, x, y):
+    return func(x, y)
+
+result = calculate(add, 4, 6)
+print(result)  # prints 10
+
+#Return a Function as a Value
+def greeting(name):
+    def hello():
+        return "Hello, " + name + "!"
+    return hello
+
+greet = greeting("Atlantis")
+print(greet())  # prints "Hello, Atlantis!"
+
+# Output: Hello, Atlantis!
+
+#Python Decorators
+def make_pretty(func):
+    # define the inner function 
+    def inner():
+        # add some additional behavior to decorated function
+        print("I got decorated")
+
+        # call original function
+        func()
+    # return the inner function
+    return inner
+
+# define ordinary function
+def ordinary():
+    print("I am ordinary")
+    
+# decorate the ordinary function
+decorated_func = make_pretty(ordinary)
+
+# call the decorated function
+decorated_func()
+
+#@ Symbol With Decorator
+def make_pretty(func):
+
+    def inner():
+        print("I got decorated")
+        func()
+    return inner
+
+@make_pretty
+def ordinary():
+    print("I am ordinary")
+
+ordinary()  
+
+#Decorating Functions with Parameters
+#'The above decorator was simple and it only worked with functions that did not have any parameters. What if we had functions that took in parameters like:'
+#This function has two parameters, a and b. We know it will give an error if we pass in b as 0.
+#Now let's make a decorator to check for this case that will cause the error.
+def smart_divide(func):
+    def inner(a, b):
+        print("I am going to divide", a, "and", b)
+        if b == 0:
+            print("Whoops! cannot divide")
+            return
+
+        return func(a, b)
+    return inner
+
+@smart_divide
+def divide(a, b):
+    print(a/b)
+
+divide(2,5)
+
+divide(2,0)
+
+#Chaining Decorators in Python
+def star(func):
+    def inner(*args, **kwargs):
+        print("*" * 15)
+        func(*args, **kwargs)
+        print("*" * 15)
+    return inner
+
+
+def percent(func):
+    def inner(*args, **kwargs):
+        print("%" * 15)
+        func(*args, **kwargs)
+        print("%" * 15)
+    return inner
+
+
+@star
+@percent
+def printer(msg):
+    print(msg)
+
+printer("Hello")
+
+#The above syntax of,
+
+@star
+@percent
+def printer(msg):
+    print(msg)
+
+#is equivalent to
+def printer(msg):
+    print(msg)
+printer = star(percent(printer))
+
+#https://www.programiz.com/python-programming/decorator
